83c5d9dc3bbbfbfb555473bba98c986d151ef780
sifatullahsu <personal.sifat@gmail.com>
2023-02-11 12:53:14 +0600

    doctors api

--- routes/doctor_handler.js
+++ routes/doctor_handler.js
@@ -6,5 +6,81 @@ const router = express.Router();
 const Doctor = new mongoose.model('Doctor', doctorSchema);
 
 
+router.post('/create', async (req, res) => {
+  const newDocument = new Doctor(req.body);
+  await newDocument.save((err) => {
+    if (err) {
+      res.json({
+        status: false,
+        message: 'There was a server side error!'
+      });
+    }
+    else {
+      res.json({
+        status: true,
+        message: 'Item created successful!'
+      })
+    }
+  });
+});
+
+
+router.patch('/edit/:id', async (req, res) => {
+
+  const query = { _id: req.params.id }
+  const result = await Doctor.updateOne(query, { $set: req.body });
+
+  if (result?.acknowledged) {
+    res.json({
+      status: true,
+      message: 'Items updated successful!'
+    });
+  }
+  else {
+    res.json({
+      status: false,
+      message: 'There was a server side error!'
+    });
+  }
+});
+
+
+router.get('/list', async (req, res) => {
+
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 10;
+  const skip = (page - 1) * size;
+
+  try {
+    const query = {}
+    const results = await Doctor.find(query).skip(skip).limit(size).sort({ _id: -1 });
+
+    res.send(response(true, results));
+  }
+  catch (error) {
+    res.send(response(false, 'There have server side error!'));
+  }
+
+});
+
+
+router.get('/single/:id', async (req, res) => {
+
+  try {
+    const { id } = req.params;
+    const query = { _id: id }
+    const results = await Doctor.findOne(query);
+
+    res.send(results ? response(true, results) : response(false, 'Data not found!'));
+  }
+  catch (error) {
+    res.send(response(false, 'There have server side error!'));
+  }
+
+});
+
+
+// /doctors/name/:name
+
 
 module.exports = router;
