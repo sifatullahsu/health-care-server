f4654004c00915273e83abfe3011c20e9634126b
sifatullahsu <personal.sifat@gmail.com>
2023-02-11 22:52:54 +0600

    booking create api

--- routes/booking_handler.js
+++ routes/booking_handler.js
@@ -6,7 +6,23 @@ const router = express.Router();
 const Booking = new mongoose.model('Booking', bookingSchema);
 
 
-
+router.post('/create', async (req, res) => {
+  const newDocument = new Booking(req.body);
+  await newDocument.save((err) => {
+    if (err) {
+      res.json({
+        status: false,
+        message: 'There was a server side error!'
+      });
+    }
+    else {
+      res.json({
+        status: true,
+        message: 'Item created successful!'
+      })
+    }
+  });
+});
 
 
 module.exports = router;
